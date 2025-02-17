918411583d59dae5062084b35f788bd0fa8ecf0a
Dorin Miron <38944987+mirondorin@users.noreply.github.com>
2025-02-17 23:54:44 +0200

    Added signal bus  (#3)
    * Signals and shit

--- Scripts/Airplane.gd
+++ Scripts/Airplane.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 class_name Airplane
 
-@export var air_speed = 800 #km/h
+@export var air_speed = 5000 #km/h
 @export var screen_speed = 0
 @export var start_distance = 100000 #km
 @export var distance = 0
@@ -13,6 +13,7 @@ class_name Airplane
 @export var markers = [] 
 var screen_start
 var screen_end
+var in_interception: bool
 
 signal plane_clicked
 
@@ -24,7 +25,10 @@ func constructor(_markers: Array, _distance: float = -1) -> void:
 	else: distance = _distance
 
 func _ready() -> void:
-	pass
+	in_interception = false
+	#SignalBus.plane_entered_interception.connect(_on_entered_interception)
+	#SignalBus.plane_exited_interception.connect(_on_exit_interception)
+	
 
 func _process(delta: float) -> void:
 	var km_per_second = air_speed / 3600.0
@@ -36,7 +40,8 @@ func move_to_marker(marker: Node2D) -> void:
 	
 func update_screen_position() -> void:
 	if screen_start && screen_end:
-		var mapped_pos = remap(distance, start_distance, 0, screen_start, screen_end)
+		var mapped_pos = remap(distance, start_distance, 0, screen_start, 
+			screen_end)
 		position = Vector2(mapped_pos, position.y)
 
 func move_to(pos: Vector2) -> void:
@@ -45,3 +50,8 @@ func move_to(pos: Vector2) -> void:
 
 func _on_button_pressed() -> void:
 	print(emit_signal("plane_clicked"))
+
+
+func set_intercepted(interception: bool) -> void:
+	print("Setting in interception to " + str(interception))
+	in_interception = interception
